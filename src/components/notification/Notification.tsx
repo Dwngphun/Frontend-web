7c110699a07135ca0a74e505ff227ff782e4925d
Tuấn Anh Võ <AnhVKT.B24CC029@stu.ptit.edu.vn>
2025-12-09 01:49:38 +0700

    Tuấn Anh update

--- src/components/notification/Notification.tsx
+++ src/components/notification/Notification.tsx
@@ -5,25 +5,20 @@ interface NotificationProps {
   type: 'success' | 'error';
   onClose: () => void; // Hàm callback để báo cho component cha là "Tôi đã đóng"
 }
-
 const Notification: React.FC<NotificationProps> = ({ message, type, onClose }) => {
-  
-  // 2. Tự động đóng sau 5 giây
+  // Tự động đóng sau 5 giây
   useEffect(() => {
     // Tạo một bộ đếm thời gian
     const timer = setTimeout(() => {
-      onClose(); // Gọi hàm onClose
+      onClose();
     }, 5000);
     //Hủy bộ đếm nếu component bị gỡ
     return () => clearTimeout(timer);
   }, [onClose]);
-  // 3. Giao diện (JSX)
   return (
-    // Áp dụng class CSS dựa trên loại thông báo
     <div className={`notification notification-${type}`}>
       {message}
     </div>
   );
 };
-
 export default Notification;
